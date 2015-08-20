5a7540a3ef2b45b63b6abf133ef070bfecb76e15
tracktopell <tracktopell@gmail.com>
2015-08-19 23:08:23 -0500

    xx

--- pmarlen-web-ligth/src/main/java/com/pmarlen/jsf/ProductoMB.java
+++ pmarlen-web-ligth/src/main/java/com/pmarlen/jsf/ProductoMB.java
@@ -14,13 +14,13 @@ import org.apache.log4j.Logger;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import org.primefaces.event.SelectEvent;
 
 @ManagedBean(name="productoMB")
-@SessionScoped
+@ViewScoped
 public class ProductoMB  {
 	private transient final Logger logger = Logger.getLogger(ProductoMB.class.getSimpleName());
 	List<ProductoQuickView> entityList;
