c1eb559835c80b8ced041c83ec8f797eb9f35d77
tracktopell <tracktopell@gmail.com>
2016-06-17 02:44:02 -0500

    fix desglose factura

---
+++ pmarlen-web-ligth/src/main/java/com/pmarlen/jsf/HistoricoTraspasosSucMB.java
@@ -0,0 +1,90 @@
+package com.pmarlen.jsf;
+
+import com.pmarlen.jsf.model.EntradaSalidaLazyDataModel;
+import com.pmarlen.model.Constants;
+import java.io.Serializable;
+
+import org.apache.log4j.Logger;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name="historicoTraspasosSucMB")
+@SessionScoped
+public class HistoricoTraspasosSucMB  implements Serializable {
+	private transient static Logger logger = Logger.getLogger(HistoricoTraspasosSucMB.class.getSimpleName());
+	
+	private boolean soloSurtidos;
+	
+	@ManagedProperty(value = "#{editarTraspasoSucMB}")
+	private EditarTraspasoSucMB editarTraspasoSucMB;
+	
+	private EntradaSalidaLazyDataModel lazyModel;	private int viewRows;
+	
+	@PostConstruct
+	public void init(){
+		logger.trace("init:");
+		soloSurtidos = false;
+		lazyModel = new EntradaSalidaLazyDataModel(Constants.TIPO_MOV_SALIDA_TRASPASO,1,soloSurtidos);
+		viewRows = 25;
+		
+	}
+	
+	public void refrescar(){
+		logger.trace("refrescar:");
+		lazyModel = new EntradaSalidaLazyDataModel(Constants.TIPO_MOV_SALIDA_TRASPASO,1,soloSurtidos);
+	}
+
+	public void setEditarTraspasoSucMB(EditarTraspasoSucMB editarTraspasoSucMB) {
+		this.editarTraspasoSucMB = editarTraspasoSucMB;
+	}
+	
+	public void editar(int id){
+		logger.debug("editar:id="+id);
+		editarTraspasoSucMB.editar(id);
+	}
+	public String editarPedidoAction(int pedidoVentaId){
+		logger.trace("editarPedidoAction:pedidoVentaId="+pedidoVentaId);
+		return editarTraspasoSucMB.editar(pedidoVentaId);
+	}
+		
+	public int getSizeList(){
+		logger.trace("getSizeList()");
+		return lazyModel.getRowCount();
+	}
+
+	public int getViewRows() {
+		logger.trace("getViewRows()");
+		return viewRows;
+	}
+
+	public void setViewRows(int viewRows) {
+		logger.trace("setViewRows("+viewRows+")");
+		this.viewRows = viewRows;
+	}
+
+	public String getImporteMoneda(double f){
+		return Constants.getImporteMoneda(f);
+	}
+
+	public EntradaSalidaLazyDataModel getLazyModel() {
+		return lazyModel;
+	}
+
+	/**
+	 * @return the soloSurtidos
+	 */
+	public boolean isSoloSurtidos() {
+		return soloSurtidos;
+	}
+
+	/**
+	 * @param soloSurtidos the soloSurtidos to set
+	 */
+	public void setSoloSurtidos(boolean soloSurtidos) {
+		this.soloSurtidos = soloSurtidos;
+	}
+
+	
+}
