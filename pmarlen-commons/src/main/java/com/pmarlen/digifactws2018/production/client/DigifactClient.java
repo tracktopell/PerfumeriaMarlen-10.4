eb10507d9f81e2b47b888b0cca7b3492ff31e905
Alfredo Estrada <Alfredo Estrada@DESKTOP-NQH9GJQ.mshome.net>
2018-01-04 12:41:24 -0600

    update sicofi client, adding TipodeComprobante=I to request

--- pmarlen-commons/src/main/java/com/pmarlen/digifactws2018/production/client/DigifactClient.java
+++ pmarlen-commons/src/main/java/com/pmarlen/digifactws2018/production/client/DigifactClient.java
@@ -72,7 +72,7 @@ public class DigifactClient {
         
 		datosCFD.setSerie(serie);
 		datosCFD.setFormadePago(pedidoVenta.getFormaDePagoDescripcion().toUpperCase());
-		datosCFD.setTipodeComprobante("F");
+		datosCFD.setTipodeComprobante("I");
         datosCFD.setMoneda("MXN");
 		
 		String mpOrig = pedidoVenta.getMetodoDePagoDescripcion().toUpperCase();
